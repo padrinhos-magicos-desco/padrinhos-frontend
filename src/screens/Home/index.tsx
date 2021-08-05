00ac47df2de46cb6db6d9c3f044fc5ad8d623cbb
João Henrique Franco <joaohenriquef@poli.ufrj.br>
2021-08-05 12:47:39 -0300

    Remove loading

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,30 +1,10 @@
-import { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import LoadingContext from '../../contexts/loading';
-import Loading from '../../components/loading';
-import { getData } from '../../gateways/api';
 import Logo from '../../components/logo';
 
 import './home.css';
 
 const Home: React.FC = () => {
-  const { loading, dispatch } = useContext(LoadingContext);
-
-  useEffect(() => {
-    async function AsyncMethod() {
-      dispatch({ type: 'SET_LOADING_STATE' });
-      await getData();
-      dispatch({ type: 'REMOVE_LOADING_STATE' });
-    }
-
-    AsyncMethod();
-  }, [dispatch]);
-
-  if (loading) {
-    return <Loading loading />;
-  }
-
   return (
     <>
       <section className="Home_section Home_main-section">
