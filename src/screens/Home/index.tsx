517ac65943f09d32177f0709c40e717447ab79a3
João Henrique Franco <joaohenriquef@poli.ufrj.br>
2021-08-05 12:10:16 -0300

    Add home structure

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,5 @@
 import { useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-
+// import { Link } from 'react-router-dom';
 import LoadingContext from '../../contexts/loading';
 import Loading from '../../components/loading';
 
@@ -25,16 +24,26 @@ const Home: React.FC = () => {
   }
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/screen/100">Screen With Param</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <section className="home-screen-section home-screen-main">
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/screen/100">Screen With Param</Link>
+            </li>
+          </ul>
+        </nav> */}
+      </section>
+      <section className="home-screen-section home-screen-secondary" />
+      <section className="home-screen-section home-screen-benefits" />
+      <section className="home-screen-section home-screen-get-to-know" />
+      <section className="home-screen-section home-screen-social-proof" />
+      <section className="home-screen-section home-screen-last-call" />
+      <section className="home-screen-section home-screen-footer" />
+    </>
   );
 };
 
