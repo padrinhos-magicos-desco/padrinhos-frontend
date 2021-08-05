9b5a7f00981e970856c88ae309376c0e730b67b5
João Henrique Franco <joaohenriquef@poli.ufrj.br>
2021-08-05 12:44:10 -0300

    Add description

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -39,6 +39,17 @@ const Home: React.FC = () => {
             </li>
           </ul>
         </nav>
+        <div className="Home_content">
+          <div className="Home_main-text">
+            <h1 className="Home_h1">PADRINHOS MÁGICOS ATIVAR!</h1>
+            <h2 className="Home_h2">
+              Um subtítulo que contextualize o rolê do apadrinhamento e como
+              isso é importante para a educação. Que somos uma empresa foda pra
+              caralho e vamos mudar a educação do mundo.
+            </h2>
+          </div>
+          <div className="Home_image" />
+        </div>
       </section>
       <section className="Home_section Home_secondary-section" />
       <section className="Home_section Home_benefits-section" />
