4b3584a9aa5c7c161efbd834b151267773ed2d1d
Gabriel Maia <gabriel.nmaia@gmail.com>
2021-08-05 19:08:31 -0300

    Add section into LastCall component

--- src/screens/Home/sections/last-call/index.tsx
+++ src/screens/Home/sections/last-call/index.tsx
@@ -4,24 +4,27 @@ import './last-call.css';
 
 const LastCall: React.FC = () => {
   return (
-    <div className="LastCall_container">
-      <h1>
-        Um título bem chamativo de last call. Não pode perder essa oportunidade
-      </h1>
-      <div className="LastCall_subtitle">
-        <p>
-          Um subtítulo que ajude esse título bem maneiro de last call. Trazer
-          pessoas uhul. Projeto 20k
-        </p>
+    <section>
+      <div className="LastCall_container">
+        <h1>
+          Um título bem chamativo de last call. Não pode perder essa
+          oportunidade
+        </h1>
+        <div className="LastCall_subtitle">
+          <p>
+            Um subtítulo que ajude esse título bem maneiro de last call. Trazer
+            pessoas uhul. Projeto 20k
+          </p>
+        </div>
+        <Button
+          buttonText="Quero participar"
+          buttonColor="#00e88f"
+          borderColor="#00e88f"
+          buttonTextColor="#000"
+          url="/screen/100"
+        />
       </div>
-      <Button
-        buttonText="Quero participar"
-        buttonColor="#00e88f"
-        borderColor="#00e88f"
-        buttonTextColor="#000"
-        url="/screen/100"
-      />
-    </div>
+    </section>
   );
 };
 
