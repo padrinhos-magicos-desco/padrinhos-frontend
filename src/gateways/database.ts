ba0066284dcba23ee270cb158a877b1a9723a730
Daniel Medina <danielmedina.rj@gmail.com>
2021-08-06 09:57:07 -0300

    adding sponsorship methods

--- src/gateways/database.ts
+++ src/gateways/database.ts
@@ -1,15 +1,19 @@
 import { Sponsor } from '../domain/sponsor';
 import Sponsored from '../domain/sponsored';
+import { Sponsorship } from '../domain/sponsorship';
 
 class Database {
   private Sponsors: Array<Sponsor> = [];
 
   private Sponsoreds: Array<Sponsored> = [];
 
+  private Sponsorships: Array<Sponsorship> = [];
+
   public static getDatabase(): Database {
     return new Database();
   }
 
+  // #region sponsors methods
   public getSponsors(): Array<Sponsor> {
     return this.Sponsors;
   }
@@ -25,7 +29,9 @@ class Database {
   public removeSponsor(id: number): void {
     this.Sponsors = this.Sponsors.filter((sp) => sp.Id !== id);
   }
+  // #endregion
 
+  // #region sponsoreds methods
   public getSponsoreds(): Array<Sponsored> {
     return this.Sponsoreds;
   }
@@ -41,6 +47,25 @@ class Database {
   public removeSponsored(id: number): void {
     this.Sponsoreds = this.Sponsoreds.filter((sp) => sp.Id !== id);
   }
+  // #endregion
+
+  // #region sponsorships methods
+  public getSponsorshipsBySponsorId(id: number): Sponsorship | undefined {
+    return this.Sponsorships.find(
+      (sponsorship) => sponsorship.Sponsor.Id === id
+    );
+  }
+
+  public getSponsorshipsBySponsoredId(id: number): Sponsorship | undefined {
+    return this.Sponsorships.find(
+      (sponsorship) => sponsorship.Sponsored.Id === id
+    );
+  }
+
+  public addSponsorship(sponsorship: Sponsorship): void {
+    this.Sponsorships.push(sponsorship);
+  }
+  // #endregion
 }
 
 export default Database;
