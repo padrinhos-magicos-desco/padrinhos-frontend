4f32a6887ae987ddb524ee007d41931efb2bf786
João Henrique Franco <joaohenriquef@poli.ufrj.br>
2021-08-06 12:03:41 -0300

    Adjust routes

--- src/routes/appRouter.tsx
+++ src/routes/appRouter.tsx
@@ -1,19 +1,21 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from '../screens/Home';
 import Screen from '../screens/Screen';
-import Sponsored from '../screens/Sponsored';
-import Login from '../screens/Login';
+import SponsoredScreen from '../screens/Sponsored';
+import LoginScreen from '../screens/Login';
+import SponsorScreen from '../screens/Sponsor';
+
 import Sandbox from '../screens/Sandbox';
 
 const AppRouter = () => {
   return (
     <Router>
       <Route path="/" exact component={Home} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/apadrinhado/cadastro" exact component={Sponsored} />
-      <Route path="/padrinho/cadastro" exact component={Sponsored} />
-      <Route path="/screen/:id" exact component={Screen} />
+      <Route path="/login" exact component={LoginScreen} />
+      <Route path="/apadrinhado/cadastro" exact component={SponsoredScreen} />
+      <Route path="/padrinho/cadastro" exact component={SponsorScreen} />
       <Route path="/sandbox" exact component={Sandbox} />
+      <Route path="/screen/:id" exact component={Screen} />
     </Router>
   );
 };
