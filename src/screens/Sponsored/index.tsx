49f1992429e4e7443de90d1256d20764c5abf302
Busa <busamar@gmail.com>
2021-08-06 08:39:37 -0300

    Sponsored screen

--- src/screens/Sponsored/index.tsx
+++ src/screens/Sponsored/index.tsx
@@ -15,6 +15,8 @@ const Sponsored: React.FC = () => {
   const [phoneValue, setPhoneValue] = useState('');
   const [courseValue, setCourseValue] = useState('');
 
+  const [step, setStep] = useState(1);
+
   const handleNameInputChange = (value: string) => {
     setNameValue(value);
   };
@@ -29,6 +31,112 @@ const Sponsored: React.FC = () => {
   };
   const handleCourseInputChange = (value: string) => {
     setCourseValue(value);
+  };
+
+  const handleContinue = () => {
+    setStep(step + 1);
+  };
+
+  const renderStep1 = () => {
+    if (step !== 1) {
+      return null;
+    }
+
+    return (
+      <RegisterBox
+        imageUrl="/register-headset-image.png"
+        onContinue={handleContinue}
+      >
+        <>
+          <p className="Sponsored_p">
+            Olá! Todo mundo quer saber, como te chamamos?
+          </p>
+          <div className="Sponsored_inputs">
+            <Input
+              placeholder="Nome"
+              type="text"
+              handleChange={handleNameInputChange}
+            />
+            <Input
+              placeholder="Sobrenome"
+              type="text"
+              handleChange={handleLastNameInputChange}
+            />
+          </div>
+        </>
+      </RegisterBox>
+    );
+  };
+
+  const renderStep2 = () => {
+    if (step !== 2) {
+      return null;
+    }
+
+    return (
+      <RegisterBox
+        imageUrl="/register-headset-image.png"
+        onContinue={handleContinue}
+      >
+        <>
+          <p className="Sponsored_p">Como entramos em contato com você?</p>
+          <div className="Sponsored_inputs">
+            <Input
+              placeholder="E-mail"
+              type="email"
+              handleChange={handleEmailInputChange}
+            />
+            <Input
+              placeholder="Telefone"
+              type="tel"
+              handleChange={handlePhoneInputChange}
+            />
+          </div>
+        </>
+      </RegisterBox>
+    );
+  };
+
+  const renderStep3 = () => {
+    if (step !== 3) {
+      return null;
+    }
+
+    return (
+      <RegisterBox
+        imageUrl="/register-headset-image.png"
+        onContinue={handleContinue}
+      >
+        <>
+          <p className="Sponsored_p">Qual curso você gostaria de fazer?</p>
+          <div className="Sponsored_dropdown">
+            <Dropdown handleChange={handleCourseInputChange} />
+          </div>
+        </>
+      </RegisterBox>
+    );
+  };
+
+  const renderStep4 = () => {
+    if (step !== 4) {
+      return null;
+    }
+
+    return (
+      <RegisterBox
+        imageUrl="/register-headset-image.png"
+        onContinue={handleContinue}
+      >
+        <>
+          <p>
+            nome: {nameValue} {lastNameValue}
+          </p>
+          <p>email: {emailValue}</p>
+          <p>telefone: {phoneValue}</p>
+          <p>curso: {courseValue}</p>
+        </>
+      </RegisterBox>
+    );
   };
 
   return (
@@ -53,72 +161,10 @@ const Sponsored: React.FC = () => {
           />
         </nav>
       </div>
-      <RegisterBox
-        imageUrl="/register-headset-image.png"
-        onContinue={() => null}
-      >
-        <>
-          <p className="Sponsored_p">
-            Olá! Todo mundo quer saber, como te chamamos?
-          </p>
-          <div className="Sponsored_inputs">
-            <Input
-              placeholder="Nome"
-              type="text"
-              handleChange={handleNameInputChange}
-            />
-            <Input
-              placeholder="Sobrenome"
-              type="text"
-              handleChange={handleLastNameInputChange}
-            />
-          </div>
-        </>
-      </RegisterBox>
-      <RegisterBox
-        imageUrl="/register-headset-image.png"
-        onContinue={() => null}
-      >
-        <>
-          <p className="Sponsored_p">Como entramos em contato com você?</p>
-          <div className="Sponsored_inputs">
-            <Input
-              placeholder="E-mail"
-              type="email"
-              handleChange={handleEmailInputChange}
-            />
-            <Input
-              placeholder="Telefone"
-              type="tel"
-              handleChange={handlePhoneInputChange}
-            />
-          </div>
-        </>
-      </RegisterBox>
-      <RegisterBox
-        imageUrl="/register-headset-image.png"
-        onContinue={() => null}
-      >
-        <>
-          <p className="Sponsored_p">Qual curso você gostaria de fazer?</p>
-          <div className="Sponsored_dropdown">
-            <Dropdown handleChange={handleCourseInputChange} />
-          </div>
-        </>
-      </RegisterBox>
-      <RegisterBox
-        imageUrl="/register-headset-image.png"
-        onContinue={() => null}
-      >
-        <>
-          <p>
-            nome: {nameValue} {lastNameValue}
-          </p>
-          <p>email: {emailValue}</p>
-          <p>telefone: {phoneValue}</p>
-          <p>curso: {courseValue}</p>
-        </>
-      </RegisterBox>
+      {renderStep1()}
+      {renderStep2()}
+      {renderStep3()}
+      {renderStep4()}
     </div>
   );
 };
